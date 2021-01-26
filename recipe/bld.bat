df4ad016d57818b28a28c44d1d6635ff48731eaa
xoviat <49173759+xoviat@users.noreply.github.com>
2021-01-26 11:42:35 -0600

    try set path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,8 @@ cd build
 
 powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 
-refreshenv
+set "path=%path%;C:\ProgramData\chocolatey\bin"
+
 choco install -y sed gawk
 
 set "PROCESSOR_ARCHITECTURE=AMD64"
