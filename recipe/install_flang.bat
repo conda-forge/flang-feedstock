3799765434f178018d5f1dd07d81bf89db2035db
xoviat <49173759+xoviat@users.noreply.github.com>
2021-01-26 17:37:49 -0600

    try new fix

--- recipe/install_flang.bat
+++ recipe/install_flang.bat
@@ -1,11 +1,13 @@
 cd %SRC_DIR%\build
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-cmake .. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
-if errorlevel 1 exit 1
-
 cmake --build . --target install
 if errorlevel 1 exit 1
+
+powershell -Command "cp $env:LIBRARY_PREFIX\bin\*.exe $env:LIBRARY_BIN"
+powershell -Command "cp $env:LIBRARY_PREFIX\lib\*.lib $env:LIBRARY_BIN\..\lib"
+powershell -Commnd "mkdir $env:LIBRARY_BIN\..\include\flang"
+powershell -Command "cp $env:LIBRARY_PREFIX\include\flang\*.mod $env:LIBRARY_BIN\..\include\flang"
 
 rm %LIBRARY_BIN%\libflang.dll
 rm %LIBRARY_BIN%\libflangrti.dll
