131e7c617e869a057439e5400bfcc2246354cd09
xoviat <49173759+xoviat@users.noreply.github.com>
2021-01-26 18:33:24 -0600

    try new fix

--- recipe/install_flang.bat
+++ recipe/install_flang.bat
@@ -4,10 +4,36 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd6
 cmake --build . --target install
 if errorlevel 1 exit 1
 
-powershell -Command "cp $env:LIBRARY_PREFIX\bin\*.exe $env:LIBRARY_BIN"
-powershell -Command "cp $env:LIBRARY_PREFIX\lib\*.lib $env:LIBRARY_BIN\..\lib"
-powershell -Commnd "mkdir $env:LIBRARY_BIN\..\include\flang"
-powershell -Command "cp $env:LIBRARY_PREFIX\include\flang\*.mod $env:LIBRARY_BIN\..\include\flang"
+mkdir %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/ieee_arithmetic.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/ieee_arithmetic_la.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/ieee_exceptions.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/ieee_exceptions_la.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/ieee_features.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/iso_c_binding.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/iso_fortran_env.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/omp_lib.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/omp_lib_kinds.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/__norm2.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/__norm2_i8.mod %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/include/flang/omp_lib.h %LIBRARY_BIN%\..\include\flang
+cp %SRC_DIR%/build/lib/flangADT.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/flangArgParser.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/libpgmath.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/pgmath.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/ompstub.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/libompstub.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/bin/libompstub.dll %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/flangrti.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/libflangrti.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/flang.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/libflang.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/lib/flangmain.lib %LIBRARY_BIN%\..\lib
+cp %SRC_DIR%/build/bin/libflangrti.dll %LIBRARY_BIN%
+cp %SRC_DIR%/build/bin/libflang.dll %LIBRARY_BIN%
+cp %SRC_DIR%/build/bin/flang.exe %LIBRARY_BIN%
+cp %SRC_DIR%/build/bin/flang1.exe %LIBRARY_BIN%
+cp %SRC_DIR%/build/bin/flang2.exe %LIBRARY_BIN%
 
 rm %LIBRARY_BIN%\libflang.dll
 rm %LIBRARY_BIN%\libflangrti.dll
