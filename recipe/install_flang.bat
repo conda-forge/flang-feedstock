bcc49d6f004026a9b815a08c7967f31640db6db5
xoviat <49173759+xoviat@users.noreply.github.com>
2021-01-26 19:12:05 -0600

    meta: copy from library prefix

--- recipe/install_flang.bat
+++ recipe/install_flang.bat
@@ -5,35 +5,35 @@ cmake --build . --target install
 if errorlevel 1 exit 1
 
 mkdir %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/ieee_arithmetic.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/ieee_arithmetic_la.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/ieee_exceptions.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/ieee_exceptions_la.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/ieee_features.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/iso_c_binding.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/iso_fortran_env.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/omp_lib.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/omp_lib_kinds.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/__norm2.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/__norm2_i8.mod %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/include/flang/omp_lib.h %LIBRARY_BIN%\..\include\flang
-cp %SRC_DIR%/build/lib/flangADT.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/flangArgParser.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/libpgmath.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/pgmath.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/ompstub.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/libompstub.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/bin/libompstub.dll %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/flangrti.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/libflangrti.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/flang.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/libflang.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/lib/flangmain.lib %LIBRARY_BIN%\..\lib
-cp %SRC_DIR%/build/bin/libflangrti.dll %LIBRARY_BIN%
-cp %SRC_DIR%/build/bin/libflang.dll %LIBRARY_BIN%
-cp %SRC_DIR%/build/bin/flang.exe %LIBRARY_BIN%
-cp %SRC_DIR%/build/bin/flang1.exe %LIBRARY_BIN%
-cp %SRC_DIR%/build/bin/flang2.exe %LIBRARY_BIN%
+cp %LIBRARY_PREFIX%/include/flang/ieee_arithmetic.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/ieee_arithmetic_la.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/ieee_exceptions.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/ieee_exceptions_la.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/ieee_features.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/iso_c_binding.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/iso_fortran_env.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/omp_lib.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/omp_lib_kinds.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/__norm2.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/__norm2_i8.mod %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/include/flang/omp_lib.h %LIBRARY_BIN%\..\include\flang
+cp %LIBRARY_PREFIX%/lib/flangADT.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/flangArgParser.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/libpgmath.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/pgmath.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/ompstub.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/libompstub.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/bin/libompstub.dll %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/flangrti.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/libflangrti.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/flang.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/libflang.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/lib/flangmain.lib %LIBRARY_BIN%\..\lib
+cp %LIBRARY_PREFIX%/bin/libflangrti.dll %LIBRARY_BIN%
+cp %LIBRARY_PREFIX%/bin/libflang.dll %LIBRARY_BIN%
+cp %LIBRARY_PREFIX%/bin/flang.exe %LIBRARY_BIN%
+cp %LIBRARY_PREFIX%/bin/flang1.exe %LIBRARY_BIN%
+cp %LIBRARY_PREFIX%/bin/flang2.exe %LIBRARY_BIN%
 
 rm %LIBRARY_BIN%\libflang.dll
 rm %LIBRARY_BIN%\libflangrti.dll
