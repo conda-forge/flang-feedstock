24be19f11f605158ac89f5f0d2ab9a988380e4f1
Isuru Fernando <isuruf@gmail.com>
2021-01-26 19:32:44 -0600

    remove unnecessary hacks

--- recipe/install_flang.bat
+++ recipe/install_flang.bat
@@ -4,37 +4,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd6
 cmake --build . --target install
 if errorlevel 1 exit 1
 
-mkdir %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/ieee_arithmetic.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/ieee_arithmetic_la.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/ieee_exceptions.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/ieee_exceptions_la.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/ieee_features.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/iso_c_binding.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/iso_fortran_env.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/omp_lib.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/omp_lib_kinds.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/__norm2.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/__norm2_i8.mod %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/include/flang/omp_lib.h %LIBRARY_BIN%\..\include\flang
-cp %LIBRARY_PREFIX%/lib/flangADT.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/flangArgParser.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/libpgmath.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/pgmath.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/ompstub.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/libompstub.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/bin/libompstub.dll %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/flangrti.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/libflangrti.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/flang.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/libflang.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/lib/flangmain.lib %LIBRARY_BIN%\..\lib
-cp %LIBRARY_PREFIX%/bin/libflangrti.dll %LIBRARY_BIN%
-cp %LIBRARY_PREFIX%/bin/libflang.dll %LIBRARY_BIN%
-cp %LIBRARY_PREFIX%/bin/flang.exe %LIBRARY_BIN%
-cp %LIBRARY_PREFIX%/bin/flang1.exe %LIBRARY_BIN%
-cp %LIBRARY_PREFIX%/bin/flang2.exe %LIBRARY_BIN%
-
 rm %LIBRARY_BIN%\libflang.dll
 rm %LIBRARY_BIN%\libflangrti.dll
 rm %LIBRARY_BIN%\libompstub.dll
