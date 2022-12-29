64b6169b261580f3d7ddd74a2d0d7b69a2d06bd5
H. Vetinari <h.vetinari@gmx.com>
2022-12-29 16:37:44 +1100

    switch to LLVM flang

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,13 +7,15 @@ cmake -G "Ninja" ^
     -DCMAKE_C_COMPILER=clang-cl ^
     -DCMAKE_CXX_COMPILER=clang-cl ^
     -DCMAKE_BUILD_TYPE="Release" ^
+    -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
     -DTARGET_ARCHITECTURE=AMD64 ^
-    %SRC_DIR%
-
-if errorlevel 1 exit 1
+    %SRC_DIR%\flang
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
+cmake --install .
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,13 @@
 mkdir build
 cd build
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_Fortran_COMPILER=flang \
-  ..
+cmake -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    ../flang
 
 
-make -j${CPU_COUNT}
+cmake --build .
+cmake --install .
