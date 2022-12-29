ffbf34423bc9b8549b1841b6bd42f72a4a3f133c
H. Vetinari <h.vetinari@gmx.com>
2022-12-29 17:25:04 +1100

    Clang_DIR is looking for ClangConfig.cmake, apparently

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake -G Ninja \
-    -DCLANG_DIR=$PREFIX/bin \
+    -DClang_DIR=$PREFIX/lib/cmake/clang \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
