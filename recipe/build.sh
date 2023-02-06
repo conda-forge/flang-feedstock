298247b917a5938c79313a9abbc5ed3d28c5dffa
H. Vetinari <h.vetinari@gmx.com>
2023-02-06 17:21:33 +1100

    follow upstream standalone build instructions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
+set -ex
+
 mkdir build
 cd build
 
 cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DFLANG_ENABLE_WERROR=ON \
+    -DLLVM_BUILD_MAIN_SRC_DIR=.. \
+    -DLLVM_EXTERNAL_LIT=$PREFIX/bin/lit \
+    -DLLVM_LIT_ARGS=-v \
+    -DLLVM_CMAKE_DIR=$PREFIX/lib/cmake/llvm \
+    -DCLANG_DIR=$PREFIX/lib/cmake/clang \
+    -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
     ../flang
-
 
 cmake --build .
 cmake --install .
