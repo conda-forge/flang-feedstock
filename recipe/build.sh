2903f5570bb806c61febfebf7281fb9fe349494b
H. Vetinari <h.vetinari@gmx.com>
2023-02-14 14:30:46 +1100

    collected changes while building 16.0.0.rc2
    * add lit
    
    * don't build tests
    
    * adapt bld.bat
    
    * reduce parallelism in `cmake --build`
    
    * build shared libs
    
    * use cos7 sysroot
    
    * host-depend on llvm/libclang-cpp for run-exports
    
    * use flang-new for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,19 +5,20 @@ mkdir build
 cd build
 
 cmake -G Ninja \
+    -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DFLANG_ENABLE_WERROR=ON \
-    -DLLVM_BUILD_MAIN_SRC_DIR=.. \
     -DLLVM_EXTERNAL_LIT=$PREFIX/bin/lit \
     -DLLVM_LIT_ARGS=-v \
     -DLLVM_CMAKE_DIR=$PREFIX/lib/cmake/llvm \
     -DCLANG_DIR=$PREFIX/lib/cmake/clang \
+    -DFLANG_INCLUDE_TESTS=OFF \
     -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
     ../flang
 
-cmake --build .
+cmake --build . -j1
 cmake --install .
