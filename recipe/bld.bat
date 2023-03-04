d78cce02fb3f8337e38bf92eea4ee4edeedbe5ef
H. Vetinari <h.vetinari@gmx.com>
2023-03-04 11:53:45 +1100

    collected changes while building 16.0.0.rc3
    * use CMake modules from LLVM source
    
    * set correct path for mlir-tblgen
    
    * use relative paths in bld.bat
    
    * add mlir as a build-dep when cross-compiling
    
    * don't enable -Werror on windows
    
    * need newer glibc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,8 +13,8 @@ cmake -G "Ninja" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_MODULE_PATH=../cmake/Modules ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DFLANG_ENABLE_WERROR=ON ^
     -DLLVM_BUILD_MAIN_SRC_DIR=.. ^
     -DLLVM_EXTERNAL_LIT=%LIBRARY_BIN%/lit ^
     -DLLVM_LIT_ARGS=-v ^
@@ -23,7 +23,7 @@ cmake -G "Ninja" ^
     -DFLANG_INCLUDE_TESTS=OFF ^
     -DMLIR_DIR=%LIBRARY_LIB%/cmake/mlir ^
     -DTARGET_ARCHITECTURE=AMD64 ^
-    %SRC_DIR%\flang
+    ..\flang
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
