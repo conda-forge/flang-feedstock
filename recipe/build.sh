627797cd47a86f7dbc3eb53be24001b31e631b0c
H. Vetinari <h.vetinari@gmx.com>
2023-03-04 16:02:59 +1100

    set correct path for mlir-tblgen

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,12 @@ set -ex
 mkdir build
 cd build
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DLLVM_CONFIG_PATH=$BUILD_PREFIX/bin/llvm-config -DMLIR_TABLEGEN_EXE=$BUILD_PREFIX/bin/mlir-tblgen"
+fi
+
 cmake -G Ninja \
+    ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
