0722f796cf28cab9cea8f14a326798088b809ee3
H. Vetinari <h.vetinari@gmx.com>
2023-04-17 13:56:20 +1100

    set FORTRANFLAGS in activate.sh

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,10 +1,13 @@
-export _OLD_LIBRARY_PATH=$LIBRARY_PATH
 export _OLD_CPATH=$CPATH
+export _OLD_LIBRARY_PATH=$LIBRARY_PATH
+export _OLD_FORTRANFLAGS=$FORTRANFLAGS
 
 if [ -z "$CONDA_PREFIX" ]; then
-    export LIBRARY_PATH=$LIBRARY_PATH:$PREFIX/lib
     export CPATH=$CPATH:$PREFIX/include
+    export LIBRARY_PATH=$LIBRARY_PATH:$PREFIX/lib
+    export FORTRANFLAGS="$FORTRANFLAGS -isystem ${CONDA_PREFIX}/include"
 else
-    export LIBRARY_PATH=$LIBRARY_PATH:$CONDA_PREFIX/lib
     export CPATH=$CPATH:$CONDA_PREFIX/include
+    export LIBRARY_PATH=$LIBRARY_PATH:$CONDA_PREFIX/lib
+    export FORTRANFLAGS="$FORTRANFLAGS -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
 fi
