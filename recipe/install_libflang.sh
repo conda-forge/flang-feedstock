9def80d7f2c332cf82b0c5aa4790399de911335b
H. Vetinari <h.vetinari@gmx.com>
2023-06-14 17:22:52 +1100

    back to shared libs on osx

--- recipe/install_libflang.sh
+++ recipe/install_libflang.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [[ "${target_platform}" == linux-* ]]; then
-    # copy symlink & its target, e.g. libFortranRuntime.so.16 & libFortranRuntime.so
-    cp $SRC_DIR/build/lib/libFortranRuntime*${SHLIB_EXT}* $PREFIX/lib
-    # same for libFortranDecimal
-    cp $SRC_DIR/build/lib/libFortranDecimal*${SHLIB_EXT}* $PREFIX/lib
-else
-    # try static builds on unix due to segfaults with shared ones
-    cp $SRC_DIR/build/lib/libFortranRuntime.a $PREFIX/lib
-    cp $SRC_DIR/build/lib/libFortranDecimal.a $PREFIX/lib
-fi
+# copy symlink & its target, e.g. libFortranRuntime.16.dylib & libFortranRuntime.dylib
+cp $SRC_DIR/build/lib/libFortranRuntime*${SHLIB_EXT}* $PREFIX/lib
+# same for libFortranDecimal
+cp $SRC_DIR/build/lib/libFortranDecimal*${SHLIB_EXT}* $PREFIX/lib
