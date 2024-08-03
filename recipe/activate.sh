79d0531d72987c7cfbad03073d201f8dc0e027a9
H. Vetinari <h.vetinari@gmx.com>
2024-08-03 21:47:31 +1100

    fix activation; doesn't work with enabledelayedexpansion

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,3 +1,4 @@
+export _OLD_FC=$FC
 export _OLD_CPATH=$CPATH
 export _OLD_FFLAGS=$FFLAGS
 export _OLD_LIBRARY_PATH=$LIBRARY_PATH
@@ -14,4 +15,5 @@ else
     export FORTRANFLAGS="$FORTRANFLAGS --sysroot=$CONDA_BUILD_SYSROOT"
 fi
 
+export FC="flang-new"
 export FFLAGS=$FORTRANFLAGS
