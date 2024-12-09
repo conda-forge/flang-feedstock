ee63ec941ba9f1fac6cd1b7eaad586de037f113d
H. Vetinari <h.vetinari@gmx.com>
2024-12-10 03:50:59 +1100

    build flang-rt using just-built flang

--- recipe/install_flang-rt.bat
+++ recipe/install_flang-rt.bat
@@ -1,15 +1,7 @@
 @echo on
 
-:: show CPU arch to detect slow CI agents early (rather than wait for 6h timeout)
-python -c "import numpy; numpy.show_config()"
-
 mkdir build
 cd build
-
-set "PROCESSOR_ARCHITECTURE=AMD64"
-
-:: necessary when compiling with clang (which has a native uint128 type; msvc doesn't)
-:: set "CXXFLAGS=%CXXFLAGS% -DAVOID_NATIVE_UINT128_T=1"
 
 cmake -G "Ninja" ^
     -DCMAKE_C_COMPILER=clang-cl ^
@@ -17,6 +9,8 @@ cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DCMAKE_CXX_STANDARD=17 ^
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ^
+    -DCMAKE_Fortran_COMPILER=%BUILD_PREFIX%/Library/bin/flang ^
+    -DCMAKE_Fortran_COMPILER_WORKS=yes ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_MODULE_PATH=../cmake/Modules ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%;%LIBRARY_LIB%/clang/%PKG_VERSION% ^
@@ -24,14 +18,14 @@ cmake -G "Ninja" ^
     -DLLVM_LIT_ARGS=-v ^
     -DLLVM_CMAKE_DIR=%LIBRARY_LIB%/cmake/llvm ^
     -DLLVM_DIR=%LIBRARY_LIB%/cmake/llvm ^
+    -DLLVM_ENABLE_RUNTIMES="flang-rt" ^
     -DCLANG_DIR=%LIBRARY_LIB%/cmake/clang ^
-    -DFLANG_INCLUDE_TESTS=OFF ^
     -DMLIR_DIR=%LIBRARY_LIB%/cmake/mlir ^
-    ..\flang
+    ..\runtimes
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . -j2
 if %ERRORLEVEL% neq 0 exit 1
 
-cd ..
-rmdir /s /q build
+cmake --install .
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/install_flang-rt.sh
+++ recipe/install_flang-rt.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# show CPU arch to detect slow CI agents early (rather than wait for 6h timeout)
-python -c "import numpy; numpy.show_config()"
-
 mkdir build
 cd build
 
@@ -17,6 +14,8 @@ cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_STANDARD=17 \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_Fortran_COMPILER=$BUILD_PREFIX/bin/flang \
+    -DCMAKE_Fortran_COMPILER_WORKS=yes \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_MODULE_PATH=../cmake/Modules \
     -DCMAKE_PREFIX_PATH=$PREFIX \
@@ -24,12 +23,10 @@ cmake -G Ninja \
     -DLLVM_LIT_ARGS=-v \
     -DLLVM_CMAKE_DIR=$PREFIX/lib/cmake/llvm \
     -DLLVM_DIR=$PREFIX/lib/cmake/llvm \
+    -DLLVM_ENABLE_RUNTIMES="flang-rt" \
     -DCLANG_DIR=$PREFIX/lib/cmake/clang \
-    -DFLANG_INCLUDE_TESTS=OFF \
     -DMLIR_DIR=$PREFIX/lib/cmake/mlir \
-    ../flang
+    ../runtimes
 
 cmake --build . -j2
-
-cd ..
-rm -rf build
+cmake --install .
