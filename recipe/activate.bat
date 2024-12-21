3dfcc7c332be9d82c3f18906602b718af4efd750
H. Vetinari <h.vetinari@gmx.com>
2024-12-21 13:44:24 +1100

    try without linking clang_rt manually

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -10,7 +10,6 @@ set "AR=llvm-ar.exe"
 
 :: following https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.bat
 set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld"
-set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 
 :: need to distinguish how we populate `-I` based on whether we're using conda build or not;
 :: LIBRARY_INC is not available if not, but we cannot use CONDA_PREFIX unconditionally either,
