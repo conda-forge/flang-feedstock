4d7ebb5d50ae7f60ef1cfc51475270700a0e67d9
H. Vetinari <h.vetinari@gmx.com>
2025-03-29 22:22:59 +1100

    also add compiler-rt to LDFLAGS

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -9,6 +9,7 @@ set "LD=lld-link.exe"
 set "AR=llvm-ar.exe"
 
 :: following https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.bat
+set "LDFLAGS=-Wl,-defaultlib:%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 set "FFLAGS=-Xflang --dependent-lib=%CONDA_PREFIX:\=/%/lib/clang/@MAJOR_VER@/lib/windows/clang_rt.builtins-x86_64.lib"
 set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -fms-runtime-lib=dll -fuse-ld=lld %FFLAGS%"
 
